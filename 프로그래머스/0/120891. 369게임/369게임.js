f9a2316d1d3c3b3b9dc6f0c2973e3a1bf39db8f3
Yerin Lee <yerin.lee0113@gmail.com>
2025-05-18 01:59:56 +0900

    [level 0] Title: 369게임, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -0,0 +1,12 @@
+function solution(order) {
+    var answer = 0;
+    var orderStr = order + "";
+    
+    for(let i=0 ; i<orderStr.length ; i++){
+        if(orderStr[i] == 3 || orderStr[i] == 6 || orderStr[i] == 9){
+            answer++;
+        }
+    }
+    
+    return answer;
+}
